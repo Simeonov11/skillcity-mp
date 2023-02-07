2245068f8873eb47f5922b1d9ccee3f3adc2acb7
Simo <simeonov.pa@gmail.com>
2023-02-07 11:10:54 +0000

    Books page created

---
+++ pages/books.js
@@ -0,0 +1,10 @@
+export default function BooksPage() {
+    return (
+        <div className="bg-green-100">
+            Books
+            <div>
+                <a href="/">Go to home page</a>
+            </div>
+        </div>
+    )
+}
