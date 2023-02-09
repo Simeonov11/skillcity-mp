73747ffa413f2af8ae18a5d564a26700fb7669c9
Simo <simeonov.pa@gmail.com>
2023-02-09 19:44:39 +0000

    took away the prefix api in the names in  api projects

--- pages/api/projects.js
+++ pages/api/projects.js
@@ -1,8 +1,8 @@
 export default function skillsHandler(req, res) {
     const skills = [
-        { name: "API-Web Store Pet Planet" , url: "https://www.petplanet.co.uk/", imgUrl: "assets/1.png"},
-        { name: "API-Counter React App" , url: "https://arnab-datta.github.io/counter-app/", imgUrl: "assets/2.png"},
-        { name: "API-Calculator React App" , url: "https://ahfarmer.github.io/calculator/", imgUrl: "assets/3.png"}
+        { name: "Web Store Pet Planet" , url: "https://www.petplanet.co.uk/", imgUrl: "assets/1.png"},
+        { name: "Counter React App" , url: "https://arnab-datta.github.io/counter-app/", imgUrl: "assets/2.png"},
+        { name: "Calculator React App" , url: "https://ahfarmer.github.io/calculator/", imgUrl: "assets/3.png"}
     ];
 
     setTimeout(() => {
