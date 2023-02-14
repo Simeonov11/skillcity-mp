4b0c72a64fd9b047e605a795dc3b38833881a959
Simo <simeonov.pa@gmail.com>
2023-02-14 23:28:49 +0000

    Added setTimeout to the status message at contact page

--- pages/contact.js
+++ pages/contact.js
@@ -32,6 +32,9 @@ export default function Contact () {
         setEmail("");
         setMessage("");
         setShowSuccess(true);
+        setTimeout(() => {
+            setShowSuccess(false);
+        },3000);
     } 
 
     return(
