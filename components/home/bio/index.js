a912ee74f845d6a04d557f6403180b55ab2971af
Simo <simeonov.pa@gmail.com>
2023-02-15 22:58:25 +0000

    Added padding for md screen for Home page Bio component

--- components/home/bio/index.js
+++ components/home/bio/index.js
@@ -1,7 +1,7 @@
 export default function Bio( { imgUrl, title, subtitle }) {
     return(
         <div className="bg-red-200 max-w-6xl mx-auto">
-            <div className="w-full flex flex-col items-center py-4 space-y-4 md:flex-row md:space-x-4">
+            <div className="w-full flex flex-col items-center py-4 space-y-4 md:flex-row md:space-x-4 md:p-3">
                 <div className="w-48">
                     <img src={imgUrl} className="rounded-lg" />
                 </div>
