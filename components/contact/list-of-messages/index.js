836665e7bef829d180a9cf6c9b6b65918fe597e9
Simo <simeonov.pa@gmail.com>
2023-02-15 10:48:15 +0000

    Added Mobile Responsive change on contact page grid

--- components/contact/list-of-messages/index.js
+++ components/contact/list-of-messages/index.js
@@ -15,7 +15,7 @@ export default function ListOfMessages( { isLoading, messages }) {
     }
     
     return(
-        <div className="w-full grid grid-cols-3 gap-4">
+        <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
             {messages.map((message, index) => (
                 <div key={index} className="bg-gray-200 py-3 px-6 rounded">
                     <p className="font-medium text-xl">{message.name}</p>
