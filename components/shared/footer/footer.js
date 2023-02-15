ee312dc3bf0773cac502bf2a854fb68224123119
Simo <simeonov.pa@gmail.com>
2023-02-15 23:33:44 +0000

    Edited styles for footer link, biger text and padding

--- components/shared/footer/footer.js
+++ components/shared/footer/footer.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function Footer({ href, title }) {
     return(
-        <div className="w-full max-w-6xl mx-auto mt-8">
+        <div className="w-full text-xl max-w-6xl mx-auto p-2 mt-8">
             <Link href={href}>
                 Go to {title} page
             </Link>
