c83dbb3b0e723567140898d84d53e6be49388c40
Simo <simeonov.pa@gmail.com>
2023-02-15 05:14:09 +0000

    Added loading state for the messages in contact page

--- components/contact/list-of-messages/index.js
+++ components/contact/list-of-messages/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 
 export default function ListOfMessages() {
     const [messages, setMessages] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
 
     useEffect(() => {
@@ -9,14 +10,30 @@ export default function ListOfMessages() {
     }, []);
 
     const getMessages = async () => {
+        setIsLoading(true);
         const response = await fetch(`/api/contact-messages`);
         const data = await response.json();
 
         const { messages } = data;
         
         setMessages(messages);
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 3000);
+        
     }
 
+    if (isLoading) {
+        return(
+            <div className="w-full grid grid-cols-3 gap-4">
+                <div className="bg-gray-200 py-3 px-6 rounded h-28 animate-pulse" />
+                <div className="bg-gray-200 py-3 px-6 rounded h-28 animate-pulse" />
+                <div className="bg-gray-200 py-3 px-6 rounded h-28 animate-pulse" />
+            </div>
+            
+        )
+    }
+    
     return(
         <div className="w-full grid grid-cols-3 gap-4">
             {messages.map((message, index) => (
