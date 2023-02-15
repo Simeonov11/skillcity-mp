a801eb65ce8f3ce92686cdf5175b02365ff75257
Simo <simeonov.pa@gmail.com>
2023-02-15 12:07:30 +0000

    Responsive breakpoint of Bio img, title, subtitle

--- components/home/bio/index.js
+++ components/home/bio/index.js
@@ -1,12 +1,14 @@
 export default function Bio( { imgUrl, title, subtitle }) {
     return(
-        <div className="bg-red-200 w-full flex flex-row space-x-2 p-4">
-            <div className="w-2/12">
-                <img src={imgUrl} className="rounded-lg" />
-            </div>
-            <div className="w-10/12 p-4">
-                <h1 className="text-4xl font-bold">{title}</h1>
-                <p className="text-xl text-gray-700">{subtitle}</p>
+        <div className="bg-red-200 max-w-6xl mx-auto">
+            <div className="w-full flex flex-col items-center py-4 space-y-4 md:flex-row md:space-x-4">
+                <div className="w-48">
+                    <img src={imgUrl} className="rounded-lg" />
+                </div>
+                <div className="w-full text-center md:text-left">
+                    <h1 className="text-6xl font-bold text-gray-700 sm:text-green-600 md:text-blue-600 lg:text-red-600">{title}</h1>
+                    <p className="text-xl text-gray-700">{subtitle}</p>
+                </div>
             </div>
       </div> 
     )
