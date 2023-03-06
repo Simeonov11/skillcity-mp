a51ba1b45bca2c313d322f9237c1a7636ba624df
Simo <simeonov.pa@gmail.com>
2023-03-06 21:20:27 +0000

    fixed text align at Header

--- components/shared/header/index.js
+++ components/shared/header/index.js
@@ -1,7 +1,7 @@
 export default function Header( { name }){
     return(
         <div>
-            <h1 className="font-bold text-4xl p-5 w-full max-w-6xl mx-auto">{name} Page</h1>
+            <h1 className="font-bold text-4xl text-center md:text-left p-5 w-full max-w-6xl mx-auto">{name} Page</h1>
         </div>
     )
 }
