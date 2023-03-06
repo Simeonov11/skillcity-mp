e7342e01fb2be0cd8c3583d41e7eb0202adfb09c
Simo <simeonov.pa@gmail.com>
2023-03-06 20:59:48 +0000

    fixed img_url (it was not showing the imgage)

--- pages/single-project/[projectId].js
+++ pages/single-project/[projectId].js
@@ -1,6 +1,7 @@
 import Bio from "@/components/home/bio";
 import Footer from "@/components/shared/footer/footer";
 import Header from "@/components/shared/header";
+import Content from "@/components/shared/header/content";
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react";
 
@@ -20,8 +21,8 @@ export default function SingleProject () {
         const response = await fetch(`/api/project-by-id?id=${id}`);
         const data = await response.json();
         setSingleProject(data);
-        console.log("data: ", SingleProject);
     }
+
 
     if (!SingleProject) {
         return (
@@ -30,12 +31,17 @@ export default function SingleProject () {
     }
 
     return (
-        <div className="w-full">
-            <div>{SingleProject.name}</div>
-            <div>{SingleProject.url}</div>
-            <div>{SingleProject.likes}</div>
-
-
+        <div className="bg-green-100 w-full max-w-6xl mx-auto">
+            <Header name={SingleProject.name} />
+            <Content>
+                <div className="bg-green-100 m-5">
+                    <div>{SingleProject.url}</div>
+                    <div>{SingleProject.likes} likes</div>
+                    <div>
+                        <img src={SingleProject.img_url}/>
+                    </div>
+                </div>  
+            </Content>
             <Footer href='/projects' title='Projects'/>
         </div>
         
